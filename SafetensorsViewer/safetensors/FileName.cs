02260ca9b79c010f1dd14b73d4e31e682bbb4edd
mcDandy <danda.martinek@gmail.com>
2026-06-28 23:33:31 +0200

    some cleanup

--- SafetensorsViewer/safetensors/FileName.cs
+++ SafetensorsViewer/safetensors/FileName.cs
@@ -2,49 +2,49 @@
 using System.Text;
 using System.Text.Json;
 
-public class SafetensorsLoader
+public class SafetensorsFileReader
 {
     private Dictionary<string, TensorInfo> _tensorRegistry = new Dictionary<string, TensorInfo>();
     private string _currentFilePath = string.Empty;
 
     public Dictionary<string, TensorInfo> TensorRegistry => _tensorRegistry;
 
-    public async Task<List<string>> LoadSafetensorsHeaderAsync(string filePath)
+    public SafetensorsFileReader(string filePath)
     {
         _currentFilePath = filePath;
         _tensorRegistry.Clear();
 
-        await Task.Run(() =>
+        byte[] headerLengthBytes = new byte[8];
+        ulong headerLength;
+        byte[] headerBytes;
+        using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
         {
-            using var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-
-            byte[] headerLengthBytes = new byte[8];
             fs.ReadExactly(headerLengthBytes, 0, 8);
-            ulong headerLength = BitConverter.ToUInt64(headerLengthBytes, 0);
+            headerLength = BitConverter.ToUInt64(headerLengthBytes, 0);
 
-            byte[] headerBytes = new byte[headerLength];
+            headerBytes = new byte[headerLength];
             fs.ReadExactly(headerBytes, 0, (int)headerLength);
-            string jsonText = Encoding.UTF8.GetString(headerBytes);
+        }
+        string jsonText = Encoding.UTF8.GetString(headerBytes);
 
-            long binaryDataStartOffset = 8 + (long)headerLength;
+        long binaryDataStartOffset = 8 + (long)headerLength;
 
-            using JsonDocument doc = JsonDocument.Parse(jsonText);
-            foreach (JsonProperty property in doc.RootElement.EnumerateObject())
+        using JsonDocument doc = JsonDocument.Parse(jsonText);
+        foreach (JsonProperty property in doc.RootElement.EnumerateObject())
+        {
+            if (property.Name == "__metadata__") continue;
+
+            TensorInfo info = JsonSerializer.Deserialize<TensorInfo>(property.Value.GetRawText())!;
+
+            if (info.DataOffsets != null && info.DataOffsets.Length >= 2)
             {
-                if (property.Name == "__metadata__") continue;
-
-                TensorInfo info = JsonSerializer.Deserialize<TensorInfo>(property.Value.GetRawText())!;
-
-                if (info.DataOffsets != null && info.DataOffsets.Length >= 2)
-                {
-                    info.DataOffsets[0] += binaryDataStartOffset;
-                    info.DataOffsets[1] += binaryDataStartOffset;
-                }
-
-                _tensorRegistry.Add(property.Name, info);
+                info.DataOffsets[0] += binaryDataStartOffset;
+                info.DataOffsets[1] += binaryDataStartOffset;
             }
-        });
 
-        return _tensorRegistry.Keys.OrderBy(k => k).ToList();
+            _tensorRegistry.Add(property.Name, info);
+        }
     }
+
+    public string[] Keys { get { return _tensorRegistry.Keys.ToArray(); } }
 }
