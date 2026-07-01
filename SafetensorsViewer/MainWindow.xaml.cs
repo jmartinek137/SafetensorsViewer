af61e08dcdd948aa45839d7a23199a17cb7872c1
mcDandy <danda.martinek@gmail.com>
2026-07-01 14:34:37 +0200

    attempt a t status

--- SafetensorsViewer/MainWindow.xaml.cs
+++ SafetensorsViewer/MainWindow.xaml.cs
@@ -1,15 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using Microsoft.Win32;
-using ScottPlot.WPF;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using TorchSharp;
 using TorchSharp.Utils;
-using static Tensorboard.CostGraphDef.Types;
 
 namespace SafetensorsViewer
 {
@@ -41,13 +40,16 @@ namespace SafetensorsViewer
             get {
                 return _dataMatrix;
             } set {
-                if (_dataMatrix != value) {
+                if (_dataMatrix != value)
+                {
+                    Status = "Rendering data";
                     PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(nameof(DataMatrix)));
                     _dataMatrix = value;
                     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DataMatrix)));
                     MyPlot.Reset();
                     MyPlot.Plot.ShowLegend();
-                    if (value != null) {
+                    if (value != null)
+                    {
                         if (IsHeatmapChecked)
                         {
                             var heatmap = MyPlot.Plot.Add.Heatmap(value);
@@ -57,14 +59,23 @@ namespace SafetensorsViewer
                         }
                         else if (IsHistogramChecked)
                         {
-                            var hist = ScottPlot.Statistics.Histogram.WithBinCount((int)Math.Ceiling(Math.Sqrt(_dataMatrix.Length)), torch.min(LoadedSafetensors).item<double>()-1e-10, torch.max(LoadedSafetensors).item<double>()+1e-10);
+                            var hist = ScottPlot.Statistics.Histogram.WithBinCount((int)Math.Ceiling(Math.Sqrt(_dataMatrix.Length)), torch.min(LoadedSafetensors).item<double>() - 1e-10, torch.max(LoadedSafetensors).item<double>() + 1e-10);
                             hist.AddRange(_dataMatrix.Cast<double>());
                             var histogram = MyPlot.Plot.Add.Histogram(hist);
                             MyPlot.Plot.XLabel("Value");
                             MyPlot.Plot.YLabel("Count");
                             MyPlot.Refresh();
                         }
+                        else if (IsStatisticsChecked)
+                        {
+                            var mean = torch.mean(LoadedSafetensors).item<double>();
+                            var median = torch.median(LoadedSafetensors).item<double>();
+                            var stddev = torch.std(LoadedSafetensors).item<double>();
+                            var var = torch.var(LoadedSafetensors).item<double>();
+                            MyPlot.Plot.Add.Text($"Mean: {mean:F4}\nMedian: {median:F4}\nStd Dev: {stddev:F4}\nVariance: {var:F4}", 0.05, 0.95);
+                        }
                     }
+                    Status = "Ready";
                 }
             }
         }
@@ -80,13 +91,10 @@ namespace SafetensorsViewer
                 _selectedTensorKey = value;
                 if (_selectedTensorKey != null)
                 {
+                    Status = "Loading tensor";
                     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedTensorKey)));
 
                     Dictionary<string, torch.Tensor> t = TorchSharp.PyBridge.Safetensors.LoadStateDict(tensorpath, [_selectedTensorKey]);
-                    if (!t.ContainsKey(_selectedTensorKey))
-                    {
-                        _selectedTensorKey = _selectedTensorKey + ".weight";
-                    }
                     if (t.ContainsKey(_selectedTensorKey))
                     {
                         var doubleTensor = t[_selectedTensorKey].to_type(torch.ScalarType.Float64);
@@ -95,9 +103,9 @@ namespace SafetensorsViewer
                         double[,] nativeMatrix = new double[t[_selectedTensorKey].shape.Count() > 0 ? t[_selectedTensorKey].shape[0] : 1, t[_selectedTensorKey].shape.Count() > 1 ? t[_selectedTensorKey].shape[1] : 1];
                         var targetSpan = MemoryMarshal.CreateSpan(ref nativeMatrix[0, 0], nativeMatrix.GetLength(0) * nativeMatrix.GetLength(1));
                         vv.CopyTo(targetSpan);
-
                         DataMatrix = nativeMatrix;
                     }
+                    Status = "Ready";
                 }
                 else {
                     DataMatrix = null;
@@ -206,11 +214,12 @@ namespace SafetensorsViewer
             OpenFileDialog openFileDialog = new();
             if (openFileDialog.ShowDialog() == true)
             {
+                Status = "Loading safetensors file";
                 tensorpath = openFileDialog.FileName;
                 SafetensorsFileReader sfr = new(tensorpath);
 
                 TensorKeys.Clear();
-
+                Status = "Building tensor key tree";
                 Dictionary<string, TreeViewItem> nodes = new();
                 TreeViewItem? parent;
                 foreach (string key in sfr.Keys)
@@ -239,6 +248,7 @@ namespace SafetensorsViewer
                     }
                     parent.Tag = key;
                 }
+                Status = "Ready";
             }
         }
         void TreeView_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
@@ -248,5 +258,21 @@ namespace SafetensorsViewer
                 SelectedTensorKey = selectedKey.Tag?.ToString();
             }
         }
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+
+        private string status;
+
+        public string Status { get => status; set => SetProperty(ref status, value); }
     }
 }
